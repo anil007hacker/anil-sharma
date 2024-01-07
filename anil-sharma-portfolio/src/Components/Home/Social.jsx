7bdd5ae000fa945440a9d4dd55a9a40922ffdfb0
anilsharma-007 <anil.sharma@logicmonitor.com>
2024-01-07 18:35:07 +0530

    completed home section

---
+++ anil-sharma-portfolio/src/Components/Home/Social.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/jsx-no-target-blank */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/anchor-has-content */
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home_social">
+        {/* Add Links here */}
+        <a href="" className="home_social-icon" target='_blank'>
+            <i class="uil uil-instagram"></i>
+        </a>
+        <a href="https://github.com/anil007hacker" className="home_social-icon" target='_blank'>
+            <i class="uil uil-github"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
